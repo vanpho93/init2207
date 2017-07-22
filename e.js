ecf325667600dd77d462c9d5c3c6e38f7ed1556d
Van Pho Nguyen <vanpho02@gmail.com>
2017-07-22 09:49:56 +0700

    bt

---
+++ e.js
@@ -0,0 +1,19 @@
+const index = 0;
+
+switch (index) {
+    case 0:
+        console.log('Sunday');
+        break;
+    case 1:
+        console.log('Monday');
+        break;
+    case 2:
+        console.log('Tuesday');
+        break;
+    case 3:
+        console.log('Wenesday');
+        break;
+    default:
+        console.log('Saturday');
+        break;
+}
---
+++ f.js
@@ -0,0 +1,7 @@
+//for while do-while
+
+// for (;;) {
+//     console.log(100);
+// }
+let i = 0;
+while (i < 10) console.log(++i);
